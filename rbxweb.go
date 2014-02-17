1b11f668c72eb292c6213794248f5f1213cc5570
Sam Hanson <humbagel@gmail.com>
2014-02-17 14:45:16 -0600

    fixed cyclic import

--- rbxweb.go
+++ rbxweb.go
@@ -1,14 +1,3 @@
 // The rbxweb package provides an interface to many of ROBLOX's web-based
 // services.
 package rbxweb
-
-// BaseDomain is the URL domain to which all requests will be sent.
-//
-// Subdomains are handled automatically as a part of API requests. Alternative
-// domains, such as gametest, follow a scheme that makes switching domains
-// easier:
-//
-//     BaseDomain:                  With subdomain:
-//     roblox.com               --> www.roblox.com
-//     gametest.robloxlabs.com  --> www.gametest.robloxlabs.com
-var BaseDomain = `roblox.com`
