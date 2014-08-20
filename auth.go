a53a4d46c7c086331626bbe605aa22adf6aa8558
Anaminus <Anaminus@users.noreply.github.com>
2014-08-20 06:46:57 -0500

    moved authentication functions to rbxweb.Client

--- auth.go
+++ auth.go
@@ -13,7 +13,7 @@ var ErrLoggedIn = errors.New("client is already logged in")
 
 // Login logs the client into a user account on the website. This is
 // neccessary for many API functions to properly execute.
-func Login(client *Client, username string, password string) (err error) {
+func (client *Client) Login(username string, password string) (err error) {
 	bd, err := json.Marshal(map[string]interface{}{
 		"userName":        username,
 		"password":        password,
@@ -63,7 +63,7 @@ func Login(client *Client, username string, password string) (err error) {
 }
 
 // Logout logs the client of out of the current user account.
-func Logout(client *Client) (err error) {
+func (client *Client) Logout() (err error) {
 	req, _ := http.NewRequest("POST", client.GetSecureURL(`www`, `/authentication/logout`, nil), nil)
 	resp, err := client.Do(req)
 	if err = client.AssertResp(resp, err); err != nil {
